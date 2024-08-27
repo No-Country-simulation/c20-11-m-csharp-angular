2a712e51f9c8583818cfe134b9010c3d80d38a4e
Giuliano Rosso <rossogiuliano@gmail.com>
2024-08-27 17:38:18 -0300

    Agregado el mapeo con AutoMapper

---
+++ src/TastysBackend/BLL/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Tastys.Domain;
+
+namespace Tastys.BLL;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Receta, RecetaDto>();
+        CreateMap<Usuario, UsuarioPublicDto>();
+        CreateMap<Review, ReviewDto>();
+        CreateMap<Categoria, CategoriaDto>();
+    }
+}
