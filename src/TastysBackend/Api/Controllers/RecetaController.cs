45f15be658a622bdd975c43b1dca2a1588787402
Giuliano Rosso <rossogiuliano@gmail.com>
2024-08-27 19:54:36 -0300

    Agregados RecetaController y RecetaService primitivos a modo de ejemplo

---
+++ src/TastysBackend/Api/Controllers/RecetaController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tastys.BLL;
+
+namespace Tastys.Api.Controllers;
+
+[ApiController]
+[Route("receta")]
+public class RecetaController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly RecetaService _recetaService;
+
+    public RecetaController(ILogger<WeatherForecastController> logger, RecetaService recetaService)
+    {
+        _logger = logger;
+        _recetaService = recetaService;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(500)]
+    [ProducesResponseType<RecetaDto[]>(200)]
+    public ActionResult<RecetaDto[]> Get([FromQuery] RecetasQuery queryParameters)
+    {
+        // Los queryParameters se validan automáticamente
+        // de acuerdo a las anotaciones en RecetasQuery
+
+        try
+        {
+            var recetas = _recetaService.GetAllRecetas();
+
+            return Ok(recetas);
+        }
+        catch
+        {
+            return StatusCode(500);
+        }
+    }
+}
