fc899b320c7952fbcf9e273ad14bccd4cb63f79b
Giuliano Rosso <rossogiuliano@gmail.com>
2024-08-27 17:31:43 -0300

    Creados algunos DTOs primitivos

---
+++ src/TastysBackend/BLL/DTOs/CategoriaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Tastys.BLL;
+
+public class CategoriaDto
+{
+    public int CategoriaID { get; init; }
+    
+    public required string Nombre { get; init; }
+    
+    public required string ImgUrl { get; init; }
+}
---
+++ src/TastysBackend/BLL/DTOs/RecetaDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Tastys.BLL;
+
+public record RecetaDto
+{
+    public int RecetaID { get; init; }
+
+    public required string Nombre { get; init; }
+
+    public required string Descripcion { get; init; }
+
+    public required string ImageUrl { get; init; }
+
+    public required UsuarioPublicDto Usuario { get; init; }
+
+    public required CategoriaDto[] Categorias { get; init; }
+
+    public required ReviewDto[] Reviews { get; init; }
+
+    public bool IsDeleted { get; init; }
+
+    public DateTime? create_at { get; init; }
+}
