485dfc4e6b4b955d65f6796b261b78f3620256bb
Giuliano Rosso <rossogiuliano@gmail.com>
2024-08-28 00:16:37 -0300

    Text search para /receta

--- src/TastysBackend/BLL/Services/Receta/RecetaService.cs
+++ src/TastysBackend/BLL/Services/Receta/RecetaService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tastys.BLL;
 
@@ -32,17 +33,24 @@ public class RecetaService
             .Include(receta => receta.Categorias)
             .AsQueryable();
 
-        // TODO: Full-text search, pero probablemente requiera
-        // hacer cambios en la DB para incluir un índice
+        // TODO: Full-text search en nombre + descripción, pero probablemente requiera
+        // hacer cambios en la DB para incluir un índice, porque si no es muy ineficiente
         if (!string.IsNullOrWhiteSpace(queryParameters.S))
-            query = query.Where(receta => receta.Nombre.Contains(queryParameters.S));
+        {
+            query = query
+                .Where(receta => receta.Nombre
+                    .Contains(queryParameters.S, StringComparison.CurrentCultureIgnoreCase));
+        }
 
         if (queryParameters.Offset.HasValue)
+        {
             query = query.Skip(queryParameters.Offset.Value);
+        }
 
         if (queryParameters.Length.HasValue)
             query = query.Take(queryParameters.Length.Value);
 
+        // TODO: Falta filtro Reviews_Length
 
         return query
             .Select(receta => _mapper.Map<RecetaDto>(receta))
