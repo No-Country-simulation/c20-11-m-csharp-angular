7a12f72a599218f3a63b9bd76aa6048cd03f0a3a
Giuliano Rosso <rossogiuliano@gmail.com>
2024-08-27 22:55:04 -0300

    Migracion para incluir a ImgUrl en Categoria

---
+++ src/TastysBackend/Infrastructure/Migrations/20240828014820_add_imgurl_to_categoria.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tastys.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_imgurl_to_categoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImgUrl",
+                table: "Categorias",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImgUrl",
+                table: "Categorias");
+        }
+    }
+}
