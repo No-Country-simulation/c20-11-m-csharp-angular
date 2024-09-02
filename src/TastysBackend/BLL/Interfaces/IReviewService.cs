bb50b40458c3ce5baf3191f6a2f1e3ac4a8f26a3
Carlos Aragon <aragoncarlosfernando@gmail.com>
2024-09-02 10:37:15 -0300

    reviewController3

--- src/TastysBackend/BLL/Interfaces/IReviewService.cs
+++ src/TastysBackend/BLL/Interfaces/IReviewService.cs
@@ -10,13 +10,11 @@ namespace Tastys.BLL.Interfaces
     internal interface IReviewService
     {
         Task<List<ReviewDto>> GetAllReview();
-        //public List <ReviewDto> GetAll();
-        // public ReviewDto GetID(int ID);
-        //Task<ReviewDto> GetReviewByID(int ID);
         public ReviewDto GetReviewById(int id);
         public ReviewDto IsDeleteReview(int id);
         public ReviewDto PutReview(Review review);
-
-
     }
 }
+
+       
+
