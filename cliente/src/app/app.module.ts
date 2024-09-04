fed4bf5788b95943f3f87c0bb7f5c79897dda8aa
Petter0915 <petter0915@gmail.com>
2024-09-05 00:26:20 +0200

    Landing page - actualizaciones header, body, navbar

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from "./componentes/header/header.component";
+import { NavBarComponent } from './componentes/navBar/navBar.component';
+import { BodyComponent } from './componentes/body/body.component';
 
 @NgModule({
   declarations: [
@@ -10,8 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    HeaderComponent,
+    NavBarComponent,
+    BodyComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
