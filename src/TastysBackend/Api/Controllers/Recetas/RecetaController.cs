e0da974b9312b2c9688d445d5ebfec26e2b3a2c5
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-09-14 20:49:45 -0300

    getorderrecetas

--- src/TastysBackend/Api/Controllers/Recetas/RecetaController.cs
+++ src/TastysBackend/Api/Controllers/Recetas/RecetaController.cs
@@ -9,44 +9,61 @@ using Swashbuckle.AspNetCore.Filters;
 namespace Tastys.API.Controllers.Recetas
 {
 
-[Route("/api/receta")]
-[ApiController]
-public class RecetaController : ControllerBase
-{
-    private readonly ILogger<RecetaController> _logger;
+    [Route("/api/receta")]
+    [ApiController]
+    public class RecetaController : ControllerBase
+    {
+        private readonly ILogger<RecetaController> _logger;
         private readonly RecetaCRUD _recetaService;
 
         public RecetaController(ILogger<RecetaController> logger, RecetaCRUD recetaService)
-    {
-        _logger = logger;
-        _recetaService = recetaService;
-    }
-
-       [HttpGet]
+        {
+            _logger = logger;
+            _recetaService = recetaService;
+        }
+        [HttpGet("order")]
+        public async Task<ActionResult<List<RecetaDto>>> GetOrderRecetas([FromQuery] int page=0,[FromQuery] int pageSize = 10,[FromQuery] QueryOrdersRecetas sort_by = QueryOrdersRecetas.Fav)
+        {
+            try
+            {
+                List<RecetaDto> recetas = await _recetaService.GetOrderRecetas(page, pageSize,sort_by);
+                
+                return Ok(recetas);
+            
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError("Error al traer recetas ordenadas: "+ex);
+                return StatusCode(500);
+                throw;
+            }
+        }
+        [HttpGet]
         public async Task<ActionResult<List<RecetaDto>>> GetAllRecetas()
-       {
-         // Los queryParameters se validan automáticamente
-          // de acuerdo a las anotaciones en RecetasQuery
+        {
+            // Los queryParameters se validan automáticamente
+            // de acuerdo a las anotaciones en RecetasQuery
 
-          try
-          {
+            try
+            {
                 List<RecetaDto> recetas = await _recetaService.GetAllRecetas();
 
-              return Ok(recetas);
-          }
-              catch (Exception ex)
-              {
-                  _logger.LogError(ex, "Error al traer recetas desde DB");
-                  return StatusCode(500);
-              }
+                return Ok(recetas);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al traer recetas desde DB");
+                return StatusCode(404);
+            }
         }
         [HttpGet(":id")]
         public async Task<ActionResult<RecetaDto>> GetRecetaByID(int ID)
         {
             try
             {
-                var receta = await  _recetaService.GetRecetaByID(ID);
-                if(receta == null){
+                var receta = await _recetaService.GetRecetaByID(ID);
+                if (receta == null)
+                {
                     return NotFound();
                 }
                 return Ok(receta);
@@ -59,17 +76,18 @@ public class RecetaController : ControllerBase
         }
         [HttpPost]
         [SwaggerRequestExample(typeof(NewRecetaDTO), typeof(RecetaRequestExample))]
-        public async Task<ActionResult<Receta>> CreateReceta([FromBody]NewRecetaDTO recetaData)
+        public async Task<ActionResult<Receta>> CreateReceta([FromBody] NewRecetaDTO recetaData)
         {
             try
             {
                 Receta postReceta = await _recetaService.CreateReceta(
-                    new Receta {
+                    new Receta
+                    {
                         Nombre = recetaData.receta.nombre,
                         ImageUrl = recetaData.receta.imageUrl,
                         Descripcion = recetaData.receta.descripcion
                     }
-                    ,recetaData.list_c,recetaData.user_id);
+                    , recetaData.list_c, recetaData.user_id);
 
                 //te retorna el codigo 201 -created- cuando se crea
                 //y ademas te dice en el header, che, encontras esta receta en la ruta /recetas/:id
@@ -78,30 +96,30 @@ public class RecetaController : ControllerBase
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error al crear la receta en DB");
-            return StatusCode(500);
+                return StatusCode(500);
+            }
         }
-    }
         [HttpDelete("{ID}")]
         public async Task<ActionResult<RecetaDto>> DeleteReceta(int ID)
         {
             try
             {
                 var receta = await _recetaService.GetRecetaByID(ID);
-                if (receta==null)
+                if (receta == null)
                 {
                     return NotFound();
                 }
                 bool deleted = await _recetaService.DeleteReceta(ID);
                 if (!deleted)
                 {
-                    return StatusCode(500,$"Error al borrar la receta con ID {ID}");
+                    return StatusCode(500, $"Error al borrar la receta con ID {ID}");
                 }
                 return Ok();
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error al borrar la receta en DB");
-                return StatusCode(500,"Error no específico, contactar a un administrador");
+                return StatusCode(500, "Error no específico, contactar a un administrador");
             }
         }
     }
