1ca020fc0386ec5312ed012bf7ba20f5d6b56bcc
gaboqu <gabrielquiroga.e@gmail.com>
2024-09-17 20:23:47 -0300

    implementacion de ingredientes - implementacion de filtro de recetas por categorias

--- cliente/src/app/servicios/receta.service.ts
+++ cliente/src/app/servicios/receta.service.ts
@@ -4,13 +4,16 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Receta } from '../model/receta.model'; // Asegúrate de tener este modelo definido
 import { API_ENDPOINT } from '../../../vars';
+import { map } from 'rxjs/operators'; // Asegúrate de importar map
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecetasService {
   private apiUrl = `${API_ENDPOINT}/api/receta`;
-  private apiUrlOrdenadas = `${API_ENDPOINT}/api/receta/order`;
+  private apiUrlOrdenadas = "https://tasty-api-staging.onrender.com/api/receta/order";
+  private apiUrlPorCategoria = "https://tasty-api-staging.onrender.com/api/categorias"; // Nuevo endpoint para filtrar por categoría
 
   constructor(private http: HttpClient) {}
 
@@ -27,4 +30,14 @@ export class RecetasService {
 
     return this.http.get<Receta[]>(this.apiUrlOrdenadas, { params });
   }
+
+ // Método para obtener recetas por categoría
+ getRecetasPorCategoria(categoriaId: number): Observable<Receta[]> {
+  const url = `${this.apiUrlPorCategoria}/${categoriaId}`;
+  return this.http.get<{ recetas: Receta[] }>(url).pipe(
+    map(response => response.recetas) // Extraemos el array de recetas de la respuesta
+  );
+}
+
+  
 }
