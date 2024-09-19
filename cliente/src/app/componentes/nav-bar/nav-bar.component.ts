0e5595e7673abe5b4c22658a25e591807b109570
gaboqu <gabrielquiroga.e@gmail.com>
2024-09-19 20:50:56 -0300

    cambio estilos

--- cliente/src/app/componentes/nav-bar/nav-bar.component.ts
+++ cliente/src/app/componentes/nav-bar/nav-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../Services/Login/AuthService';  // Ajusta la ruta según corresponda
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,6 +10,8 @@ import { Observable } from 'rxjs';
 export class NavBarComponent implements OnInit {
   isAuthenticated = false;
 
+  constructor(private router: Router) {} // Inyectar el Router
+
   ngOnInit(): void {
     this.isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
   }
@@ -17,6 +19,16 @@ export class NavBarComponent implements OnInit {
   logout(): void {
     localStorage.removeItem('isAuthenticated'); 
     localStorage.removeItem('nombre'); 
+    localStorage.removeItem('id_user'); // También puedes eliminar el ID del usuario
     this.isAuthenticated = false;
+  }
+
+  redirectToUserDashboard(): void {
+    const userId = localStorage.getItem('id_user'); // Obtener el ID del usuario del local storage
+    if (userId) {
+      this.router.navigate(['/usuarios/', userId]); // Redirigir pasando el ID
+    } else {
+      console.error('No se encontró el ID del usuario en el local storage.');
+    }
   }
 }
